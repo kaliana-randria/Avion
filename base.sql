4ad9b34741f3ca2dc74b70d023a5e908e143a164
kaliana-randria <kaliana.randriambololona@gmail.com>
2025-06-26 15:57:55 +0300

    modifier

--- base.sql
+++ base.sql
@@ -1,152 +1,110 @@
--- CREATE DATABASE gestion_avion;
--- use gestion_avion;
+-- CREATE DATABASE gestion_avion2;
+-- use gestion_avion2;
 
-CREATE TABLE avions (
+CREATE TABLE compagnie (
+    id_compagnie SERIAL PRIMARY KEY,
+    nom_compagnie VARCHAR(100) NOT NULL
+); 
+
+INSERT INTO compagnie (nom_compagnie) VALUES
+('Air Madagascar'),
+('Air France'),
+('Turkish Airlines'),
+('Ethiopian Airlines'),
+('Corsair');
+
+CREATE TABLE avion (
     id_avion SERIAL PRIMARY KEY,
-    nom VARCHAR(100) NOT NULL,
-    code VARCHAR(20) UNIQUE,
-    modele VARCHAR(50),
-    business_seats INT CHECK (business_seats >= 0),
-    economy_seats INT CHECK (economy_seats >= 0)
+    id_compagnie INT NOT NULL,
+    nom_avion VARCHAR(100) NOT NULL,
+    modele VARCHAR(50) NOT NULL,
+    FOREIGN KEY (id_compagnie) REFERENCES compagnie(id_compagnie)
 ); 
-INSERT INTO avions (nom, code, modele, business_seats, economy_seats) VALUES
-('Airbus A320', 'A320-01', 'A320', 20, 120),
-('Boeing 737', 'B737-02', '737 Max', 18, 150);
 
-CREATE TABLE statuts_vol (
+INSERT INTO avion (id_compagnie, nom_avion, modele) VALUES
+(1, 'Tsaradia 101', 'ATR 72'),
+(2, 'Airbus A320 AF', 'A320'),
+(3, 'Boeing 737 Turkish', 'B737'),
+(4, 'Dreamliner 787', 'B787'),
+(5, 'Corsair 900', 'A330');
+
+CREATE TABLE statut_vol (
     id_statut_vol SERIAL PRIMARY KEY,
-    statut VARCHAR(20)
-);
-INSERT INTO statuts_vol (statut) VALUES 
-('prévu'), ('en vol'), ('retardé'), ('annulé'), ('terminé');
+    statut VARCHAR(20) NOT NULL
+); 
 
-CREATE TABLE vols (
+INSERT INTO statut_vol (statut) VALUES
+('prevu'),
+('en vol'),
+('termine'),
+('annule');
+
+CREATE TABLE vol (
     id_vol SERIAL PRIMARY KEY,
-    numero_vol VARCHAR(10) NOT NULL,
-    compagnie VARCHAR(100) NOT NULL,
-    ville_depart VARCHAR(100) NOT NULL,
-    ville_arrivee VARCHAR(100) NOT NULL,
-    date_depart TIMESTAMP NOT NULL,
-    date_arrivee TIMESTAMP NOT NULL,
-    duree INT NOT NULL,
+    id_avion INT,
+    ville_depart VARCHAR(100),
+    ville_arrivee VARCHAR(100),
+    date_depart TIMESTAMP,
+    date_arrivee TIMESTAMP,
+    duree INT,
     id_statut_vol INT DEFAULT 1,
-    id_avion INT NOT NULL,
-    FOREIGN KEY (id_statut_vol) REFERENCES statuts_vol(id_statut_vol),
-    FOREIGN KEY (id_avion) REFERENCES avions(id_avion)
-); -- c'est crud cote admin donc dans code
+    FOREIGN KEY (id_avion) REFERENCES avion(id_avion),
+    FOREIGN KEY (id_statut_vol) REFERENCES statut_vol(id_statut_vol)
+); -- dans fonction crud
 
-CREATE TABLE classes (
+CREATE TABLE classe (
     id_classe SERIAL PRIMARY KEY,
-    nom VARCHAR(50) NOT NULL
-);
-INSERT INTO classes (nom) VALUES 
-('Economique'), ('Business');
-
-CREATE TABLE tarifs (
-    id_tarif SERIAL PRIMARY KEY,
-    id_vol INT NOT NULL,
-    id_classe INT NOT NULL,
-    quantite INT NOT NULL CHECK (quantite > 0),
-    prix DECIMAL(10,2) NOT NULL CHECK (prix >= 0),
-    pourcentage INT CHECK (pourcentage BETWEEN 0 AND 100),
-    date_limite_paiement DATE, 
-    FOREIGN KEY (id_vol) REFERENCES vols(id_vol),
-    FOREIGN KEY (id_classe) REFERENCES classes(id_classe)
-); -- crud cote admin 
-
-CREATE TABLE clients (
-    id_client SERIAL PRIMARY KEY,
-    nom_client VARCHAR(100) NOT NULL,
-    email_client VARCHAR(100),
-    mdp VARCHAR(100)
+    nom_classe VARCHAR(50)
 ); 
-INSERT INTO clients (nom_client, email_client, mdp) VALUES
-('Jean Rakoto', 'jean@gmail.com', 'mdp123'),
-('Soa Rami', 'soa@gmail.com', 'secret456');
 
-CREATE TABLE statuts_reservation (
-    id_statuts_reservation SERIAL PRIMARY KEY,
-    statut VARCHAR(20)
-);
-INSERT INTO statuts_reservation (statut) VALUES 
-('en attente'), ('payé'), ('expiré');
+INSERT INTO classe (nom_classe) VALUES
+('Economique'),
+('Business'),
+('First'),
+('VIP');
 
-CREATE TABLE reservations (
-    id_reservation SERIAL PRIMARY KEY, 
-    id_vol INT NOT NULL,
-    id_classe INT NOT NULL,
-    id_client INT,
-    quantite INT NOT NULL,
-    id_statut_reservation INT DEFAULT 1, 
+CREATE TABLE classe_vol (
+    id_classe_vol SERIAL PRIMARY KEY,
+    id_vol INT,
+    id_classe INT,
+    nbr_place INT,
+    FOREIGN KEY (id_vol) REFERENCES vol(id_vol),
+    FOREIGN KEY (id_classe) REFERENCES classe(id_classe)
+); -- depend du vol (donc est ce dans crud)
+
+CREATE TABLE param_vol (
+    id_param_vol SERIAL PRIMARY KEY,
+    id_classe_vol INT,
+    prix DECIMAL(10,2),
+    quantite INT,
+    date_limite_paiement TIMESTAMP,
+    en_cours BOOLEAN DEFAULT FALSE,
+    FOREIGN KEY (id_classe_vol) REFERENCES classe_vol(id_classe_vol)
+); -- depend du classe_vol
+
+CREATE TABLE reservation (
+    id_reservation SERIAL PRIMARY KEY,
     date_reservation TIMESTAMP,
-    FOREIGN KEY (id_vol) REFERENCES vols(id_vol),
-    FOREIGN KEY (id_classe) REFERENCES classes(id_classe),
-    FOREIGN KEY (id_client) REFERENCES clients(id_client),
-    FOREIGN KEY (id_statut_reservation) REFERENCES statuts_reservation(id_statuts_reservation)
-);-- crud dans code cote client
+    id_param_vol INT,
+    quantite INT,
+    est_payer BOOLEAN DEFAULT FALSE,
+    FOREIGN KEY (id_param_vol) REFERENCES param_vol(id_param_vol)
+); -- crud
 
-CREATE TABLE categories_age (
-    id_categorie SERIAL PRIMARY KEY,
-    type_personne VARCHAR(50), 
-    age_min INT,
-    age_max INT
-);
-INSERT INTO categories_age (type_personne, age_min, age_max) VALUES
-('bébé', 0, 1),
-('enfant', 2, 11),
-('adolescent', 12, 17),
-('jeune adulte', 18, 29),
-('adulte', 30, 59),
-('senior', 60, 120);
+CREATE TABLE enregistrement_reservation (
+    id_enregistrement SERIAL PRIMARY KEY,
+    id_reservation INT,
+    num_reference VARCHAR(10),
+    est_annule BOOLEAN DEFAULT FALSE,
+    FOREIGN KEY (id_reservation) REFERENCES reservation(id_reservation)
+); -- insertion depend du reservation
 
-CREATE TABLE passagers (
-    id_passager SERIAL PRIMARY KEY,
-    nom_passager VARCHAR(100),
-    genre_passager CHAR(1) CHECK (genre_passager IN ('M', 'F')),
-    id_categorie_age INT,
-    FOREIGN KEY (id_categorie_age) REFERENCES categories_age(id_categorie)
-); 
-INSERT INTO passagers (nom_passager, genre_passager, id_categorie_age) VALUES
-('Lala', 'F', 4),
-('Tom', 'M', 3),
-('Sarah', 'F', 2),
-('Kevin', 'M', 5),
-('Ines', 'F', 1),
-('Paul', 'M', 4),
-('Zoé', 'F', 6),
-('Noah', 'M', 3),
-('Emma', 'F', 3),
-('Lucas', 'M', 2),
-('Anna', 'F', 4),
-('Leo', 'M', 4),
-('Mia', 'F', 5),
-('Nathan', 'M', 4),
-('Eva', 'F', 6),
-('Adam', 'M', 1),
-('Lina', 'F', 3),
-('Yanis', 'M', 5),
-('Nina', 'F', 2),
-('Ali', 'M', 4),
-('Sofia', 'F', 5),
-('Rayan', 'M', 3),
-('Chloé', 'F', 6),
-('Jules', 'M', 4),
-('Naïa', 'F', 4);
-
-CREATE TABLE tickets (
-    id SERIAL PRIMARY KEY,
-    id_reservation INT NOT NULL,
-    id_passager INT NOT NULL,
-    numero_ticket VARCHAR(50) UNIQUE,
-    FOREIGN KEY (id_reservation) REFERENCES reservations(id_reservation),
-    FOREIGN KEY (id_passager) REFERENCES passagers(id_passager)
-); -- depend de la reservation donc inserer dans code
-
-CREATE TABLE admins (
+CREATE TABLE admin (
     id_admin SERIAL PRIMARY KEY,
-    nom_admin VARCHAR(100) NOT NULL,
-    email VARCHAR(100) NOT NULL,
-    mdp VARCHAR(100) NOT NULL
-);
-INSERT INTO admins (nom_admin, email, mdp) VALUES
+    nom VARCHAR(100),
+    email VARCHAR(100),
+    mdp VARCHAR(50)
+); 
+
+INSERT INTO admin (nom, email, mdp) VALUES
 ('Admin', 'admin@gmail.com', 'admin123');
